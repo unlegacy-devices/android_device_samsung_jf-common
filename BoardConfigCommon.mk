48c8074610b08a7ffc60ef708abc7e1587b6f679
dkati <dkatikaridis@gmail.com>
2022-04-03 23:30:15 +0300

    jflte: Enable some GO configs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,6 +154,11 @@ endif
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 
