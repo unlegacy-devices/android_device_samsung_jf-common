c2dc776c3ee0108089e120bc14d8268be5cc9d50
NotNoelChannel <notnoel12345678@gmail.com>
2025-08-08 11:41:28 +0200

    lcd density: Decrease and use board flag

--- system_prop.mk
+++ system_prop.mk
@@ -27,7 +27,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=c2d \
     debug.hwui.use_buffer_age=false \
     debug.sf.latch_unsignaled=1 \
-    ro.sf.lcd_density=480 \
     ro.opengles.version=196608 \
     debug.renderengine.backend=threaded \
     debug.sf.hw=1 \
